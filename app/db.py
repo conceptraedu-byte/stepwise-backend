2ddcfe54109cc469a41afd5d184282a15e7201fd
Suraj762002 <snmsss2002@gmail.com>
2026-02-02 20:36:52 +0530

    changes

--- app/db.py
+++ app/db.py
@@ -1,7 +1,10 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
-client = AsyncIOMotorClient(os.getenv("MONGO_URI"))
+mongo_uri = os.getenv("MONGO_URI")
+print("MONGO_URI FROM ENV:", mongo_uri)
+
+client = AsyncIOMotorClient(mongo_uri)
 db = client["stepwise"]
 
 users_collection = db["users"]
